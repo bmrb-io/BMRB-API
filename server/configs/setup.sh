48b9918ab511e5f1510473d81d25a386627655d2
Jon Wedell <wedell@bmrb.wisc.edu>
2016-04-18 12:29:27 -0500

    Add redis-sentinel setup instructions.

--- server/configs/setup.sh
+++ server/configs/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 yum install redis python-redis python-psycopg2
+
+# Configure redis installation
 systemctl enable redis
 systemctl start redis
-
+systemctl enable redis-sentinel
+systemctl start redis-sentinel
