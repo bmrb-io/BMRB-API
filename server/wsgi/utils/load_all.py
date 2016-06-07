0cc95e3456e5b9e17868e880c2d5daaa744a05f8
Jon Wedell <wedell@bmrb.wisc.edu>
2016-06-07 11:20:33 -0500

    Refactor load_all. Keep separate entry lists as well as combined list. Allow only refreshing one list at a time. Use optparse to parse arguments. Query for metabolomics entries from relational DB.

--- server/wsgi/utils/load_all.py
+++ server/wsgi/utils/load_all.py
@@ -5,46 +5,74 @@ import sys
 import json
 import time
 import zlib
-
+import optparse
 import querymod
 from multiprocessing import Pipe, cpu_count
 
-loaded = []
-to_process = []
+loaded = {'metabolomics': [], 'macromolecules': [], 'chemcomps': [], 'combined': []}
+to_process = {'metabolomics': [], 'macromolecules': [], 'chemcomps': []}
 
 # Load the configuration file
 configuration = querymod.configuration
 
-# Figure out which REDIS to connect to
-redis_db = 1
+# Specify some basic information about our command
+opt = optparse.OptionParser(usage="usage: %prog", version="1.0", description="Update the entries in the Redis database.")
+opt.add_option("--metabolomics", action="store_true", dest="metabolomics", default=False, help="Update the metabolomics entries.")
+opt.add_option("--macromolecules", action="store_true", dest="macromolecules", default=False, help="Update the macromolecule entries.")
+opt.add_option("--chemcomps", action="store_true", dest="chemcomps", default=False, help="Update the chemcomp entries.")
+opt.add_option("--all", action="store_true", dest="all", default=False, help="Update all the entries.")
+opt.add_option("--redis-db", action="store", dest="db", default=1, help="The Redis DB to use. 0 is master.")
+# Parse the command line input
+(options, cmd_input) = opt.parse_args()
 
-# By default put data into a "staging" db. Only put it in master if they
-#  specify the first argument as "master"
-if len(sys.argv) > 1 and sys.argv[1] == "master":
-    redis_db = 0
-    print("Operating on master DB.")
-else:
-    print("Operating on staging DB.")
+# Make sure they specify a DB
+if not (options.metabolomics or options.macromolecules or
+        options.chemcomps or options.all):
+    print("You must specify which entries to load.")
+    sys.exit(1)
 
-# Load the metabalomics data
-for one_dir in os.listdir("/share/subedit/metabolomics/"):
-    to_process.append([str(one_dir), os.path.join("/share/subedit/metabolomics/", one_dir, one_dir + ".str")])
+# Update the values if all is presented
+if options.all:
+    options.metabolomics = True
+    options.macromolecules = True
+    options.chemcomps = True
+
+if options.metabolomics and options.macromolecules and options.chemcomps:
+    options.all = True
+
+# Load the metabolomics data
+if options.metabolomics:
+    entries = querymod.get_fields_by_fields(["Entry_ID"],
+                                            "Release",
+                                            schema="metabolomics")
+    entries = sorted(set(entries["Release.Entry_ID"]))
+    for entry in entries:
+        ent = (entry, "/share/subedit/metabolomics/%s/%s.str" % (entry, entry))
+        to_process['metabolomics'].append(ent)
 
 # Get the released entries from ETS
-conn, cur = querymod.get_postgres_connection(user=configuration['ets']['user'],
-                                             host=configuration['ets']['host'],
-                                             database=configuration['ets']['database'])
-cur.execute("SELECT bmrbnum FROM entrylog;")
-all_ids = [x[0] for x in cur.fetchall()]
-cur.execute("SELECT bmrbnum FROM entrylog WHERE status LIKE 'rel%';")
-valid_ids = [x[0] for x in cur.fetchall()]
+if options.macromolecules:
+    conn, cur = querymod.get_postgres_connection(user=configuration['ets']['user'],
+                                                 host=configuration['ets']['host'],
+                                                 database=configuration['ets']['database'])
+    cur.execute("SELECT bmrbnum FROM entrylog;")
+    all_ids = [x[0] for x in cur.fetchall()]
+    cur.execute("SELECT bmrbnum FROM entrylog WHERE status LIKE 'rel%';")
+    valid_ids = [x[0] for x in cur.fetchall()]
+
+    # Load the normal data
+    for entry_id in valid_ids:
+        to_process['macromolecules'].append([str(entry_id), "/share/subedit/entries/bmr%d/clean/bmr%d_3.str" % (entry_id, entry_id)])
 
 # Load the chemcomps
-to_process.extend([[x, None] for x in querymod.list_entries(database="chemcomps")])
+if options.chemcomps:
+    chemcomps = ["chemcomp_" + x for x in querymod.get_fields_by_fields(["BMRB_code"], "Entity", schema="chemcomps")['Entity.BMRB_code']]
+    to_process['chemcomps'].extend([[x, None] for x in chemcomps])
 
-# Load the normal data
-for entry_id in valid_ids:
-    to_process.append([str(entry_id), "/share/subedit/entries/bmr%d/clean/bmr%d_3.str" % (entry_id, entry_id)])
+# Generate the flat list of entries to process
+to_process['combined'] = (to_process['metabolomics'] +
+                          to_process['macromolecules'] +
+                          to_process['chemcomps'])
 
 def one_entry(entry_name, entry_location, r):
     """ Load an entry and add it to REDIS """
@@ -78,7 +106,7 @@ def one_entry(entry_name, entry_location, r):
             return entry_name
 
 # Since we are about to start, tell REDIS it is being updated
-r = querymod.get_redis_connection(db=redis_db)
+r = querymod.get_redis_connection(db=options.db)
 
 processes = []
 num_threads = cpu_count()
@@ -95,8 +123,8 @@ for thread in xrange(0,num_threads):
     # Okay, we are the child
     if newpid == 0:
 
-        # Each child gets a REDIS
-        red = querymod.get_redis_connection(db=redis_db)
+        # Each child gets a Redis
+        red = querymod.get_redis_connection(db=options.db)
         child_conn.send("ready")
         while True:
             parent_message = child_conn.recv()
@@ -117,19 +145,32 @@ for thread in xrange(0,num_threads):
         child_conn.close()
 
 # We are starting to update
+time.sleep(1)
 r.set("ready", 0)
 
+def add_to_loaded(entry):
+    if data.startswith("chemcomp"):
+        loaded['chemcomps'].append(data)
+    elif data.startswith("bm"):
+        loaded['metabolomics'].append(data)
+    else:
+        loaded['macromolecules'].append(data)
+    loaded['combined'].append(data)
+
 # Check if entries have completed by listening on the sockets
-while len(to_process) > 0:
+while len(to_process['combined']) > 0:
 
     time.sleep(.001)
     # Poll for processes ready to listen
     for proc in processes:
         if proc[0].poll():
             data = proc[0].recv()
-            if data and data != "ready":
-                loaded.append(data)
-            proc[0].send(to_process.pop())
+            if data:
+                if data != "ready":
+                    add_to_loaded(data)
+            else:
+                print "Not loaded."
+            proc[0].send(to_process['combined'].pop())
             break
 
 # Reap the children
@@ -141,20 +182,45 @@ for thread in xrange(0, num_threads):
 
     res = os.wait()
     if data:
-        loaded.append(data)
-
-# Delete all entries that might have been withdrawn
-for x in all_ids:
-    if x not in valid_ids:
-        if r.delete(x) == 1:
-            print("Deleting entry that is no longer valid: %d" % x)
+        add_to_loaded(data)
 
 # Put a few more things in REDIS
+def make_entry_list(name, values):
+    loading = name + "_loading"
+    r.delete(loading)
 
-# Use a REDIS list so other applications can read the list of entries
-for x in sorted(loaded):
-    r.rpush("loading", x)
-r.rename("loading", "loaded")
+    for x in sorted(values):
+        r.rpush(loading, x)
+    r.rename(loading, name)
+
+def print_dropped(db):
+    dropped = [x[0] for x in to_process[db] if x[0] not in set(loaded[db])]
+    if len(dropped) > 0:
+        print("Entries not loaded in DB %s: %s" % (db, dropped))
+
+# Use a Redis list so other applications can read the list of entries
+if options.metabolomics:
+    make_entry_list('metabolomics', loaded['metabolomics'])
+    print_dropped('metabolomics')
+if options.macromolecules:
+    make_entry_list('macromolecules', loaded['macromolecules'])
+    print_dropped('macromolecules')
+if options.chemcomps:
+    make_entry_list('chemcomps', loaded['chemcomps'])
+    print_dropped('chemcomps')
+
+# Make the full list from the existing lists regardless of update type
+loaded['combined'] = (r.lrange('metabolomics', 0, -1) +
+                      r.lrange('macromolecules', 0, -1) +
+                      r.lrange('chemcomps', 0, -1))
+make_entry_list('combined', loaded['combined'])
+
+# Delete all entries that might have been withdrawn
+if options.macromolecules:
+    for x in all_ids:
+        if x not in valid_ids:
+            if r.delete(x) == 1:
+                print("Deleting entry that is no longer valid: %d" % x)
 
 # Set the time
 r.set("update_time", time.time())
