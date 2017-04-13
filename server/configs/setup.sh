33838d9603f324824b6522c2c91e131a27ce95f4
Jon Wedell <wedell@bmrb.wisc.edu>
2017-04-13 15:56:07 -0500

    Switch to flask and psycop2 from pip to get more recent version.

--- server/configs/setup.sh
+++ server/configs/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-yum install redis python-redis python-psycopg2 python-flask mod_evasive apache-gzip java-1.8.0-openjdk-headless
+yum install redis python-redis mod_evasive apache-gzip java-1.8.0-openjdk-headless
+
+sudo pip install flask psycopg2
 
 # Configure redis installation
 systemctl enable redis
