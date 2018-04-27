b91314688d6fd72fde8b172c3949715fc409ab41
Jon Wedell <wedell@bmrb.wisc.edu>
2018-04-27 13:50:46 -0500

    Update molprobity sql loading script now that a separate tablespace isn't needed.

--- server/wsgi/utils/sql/molprobity.sql
+++ server/wsgi/utils/sql/molprobity.sql
@@ -3,9 +3,6 @@
 -- Activate once psql>=9.3
 --CREATE SCHEMA IF NOT EXISTS molprobity;
 
--- create tablespace webapi location '/websites/webapi/postgres_tables';
--- grant create on tablespace webapi to bmrb;
--- revoke create on tablespace webapi from web;
 
 DROP TABLE IF EXISTS molprobity.oneline_tmp;
 CREATE table molprobity.oneline_tmp (
@@ -44,7 +41,7 @@ structure_val_oneline_list_id integer,
 macromolecule_types text,
 
     PRIMARY KEY (pdb, model, hydrogenations, molprobity_flips, backbone_trim_state)
-) TABLESPACE webapi;
+);
 
 -- Temp table with duplicates
 CREATE TEMP TABLE tmp_table
@@ -131,13 +128,13 @@ outlier_count_separate_geometry integer,
 outlier_count integer,
 entry_id text,
 structure_validation_residue_list_id integer
-) TABLESPACE webapi;
+);
 
 /* To remove duplicates:
 LC_ALL=C sort -u -i everything.csv > everything_unique.csv
 */
 \copy molprobity.residue_tmp FROM '/websites/extras/files/pdb/molprobity/residue_files/everything.csv' DELIMITER ':' CSV;
-CREATE INDEX ON molprobity.residue_tmp (pdb) TABLESPACE webapi;
+CREATE INDEX ON molprobity.residue_tmp (pdb);
 
 -- Move the new table into place
 ALTER TABLE IF EXISTS molprobity.residue RENAME TO residue_old;
