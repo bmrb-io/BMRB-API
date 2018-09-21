d5c98eda0db2d4b0f89561fcf369ddff77eb3d21
Jon Wedell <wedell@bmrb.wisc.edu>
2018-09-21 08:55:49 -0500

    Update setup.py script.

--- server/configs/setup.sh
+++ server/configs/setup.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-yum install redis python-pip python-redis mod_evasive apache-gzip java-1.8.0-openjdk-headless
+# The postgresql10-contrib package is for pg_trgm
+yum install redis python-pip python-virtualenv mod_evasive apache-gzip postgresql10-contrib
+#java-1.8.0-openjdk-headless - not sure what that was for
 
-sudo pip install -r requirements.txt
-
-# Install the postgres contribs module for pg_trgm
-sudo yum install postgresql10-contrib
+# Set up the virtualenv
+source ../wsgi/env/bin/activate
+cd ../wsgi/
+./setup_virtualenv.sh
+cd -
 
 # Configure redis installation
 systemctl enable redis
@@ -16,3 +19,4 @@ systemctl start redis-sentinel
 # Build FASTA
 cd ../wsgi/submodules/fasta36/src
 make -f ../make/Makefile.linux_sse2 all
+
