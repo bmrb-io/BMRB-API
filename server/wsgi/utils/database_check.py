98ea592d31ba771742326bd76508d92fff59d6d4
Jon Wedell <jonwedell@gmail.com>
2019-06-07 17:38:22 -0400

    Improvements to DB checker script.

--- server/wsgi/utils/database_check.py
+++ server/wsgi/utils/database_check.py
@@ -2,16 +2,10 @@
 
 """ Verify the DB load worked. """
 
-# Make sure print functions work in python2 and python3
-from __future__ import print_function
-
-import os
 import sys
-import time
-import zlib
+import logging
 import optparse
 import psycopg2
-
 
 ETS_HOST = 'torpedo'
 ETS_USER = 'ets'
@@ -23,10 +17,7 @@ def get_postgres_connection(user, host, database, port):
     """ Returns a connection to postgres and a cursor."""
 
     # Errors connecting will be handled upstream
-    conn = psycopg2.connect(user=user, host=host, database=database, port=port)
-    cur = conn.cursor()
-
-    return conn, cur
+    return psycopg2.connect(user=user, host=host, database=database, port=port).cursor()
 
 
 # Specify some basic information about our command
@@ -43,19 +34,38 @@ opt.add_option("--user", action="store", dest="user",
 # Parse the command line input
 (options, cmd_input) = opt.parse_args()
 
+logging.getLogger().setLevel(logging.DEBUG)
 
 # Get the released entries from ETS
-ets_cur = get_postgres_connection(user=ETS_USER, host=ETS_HOST, database=ETS_DB, port=ETS_PORT)[1]
+ets_cur = get_postgres_connection(user=ETS_USER, host=ETS_HOST, database=ETS_DB, port=ETS_PORT)
 ets_cur.execute("SELECT bmrbnum FROM entrylog;")
 all_ids = [x[0] for x in ets_cur.fetchall()]
 ets_cur.execute("SELECT bmrbnum FROM entrylog WHERE status LIKE 'rel%';")
 valid_ids = sorted([int(x[0]) for x in ets_cur.fetchall()])
+match_count = 0
 
+ets_cur.execute("SELECT bmrbnum from entrylog WHERE status LIKE 'rel%' AND !!!!!!!!!!!;")
+recent_ids = ets_cur.fetchall()
 
-db_cur = get_postgres_connection(user=options.user, host=options.host, database=options.database, port=options.port)[1]
+db_cur = get_postgres_connection(user=options.user, host=options.host, database=options.database, port=options.port)
 
 # Check that each of these entries has a record in the "Entry" table
+for entry in recent_ids:
+    logging.debug('Checking recent release %s' % entry)
+    db_cur.execute('''SELECT "ID" FROM macromolecules."Entry" where entry= %s''', [entry])
+    if len(db_cur.fetchall()) < 1:
+        logging.exception('Recently released entry %s was not found in the DB!' % entry)
+        sys.exit(1)
+
+logging.info('Checking all entries are released.')
+unreleased = set()
 for entry in valid_ids:
-    print('Checking %s' % entry)
-    db_cur.execute('''SELECT bmrbid from entry where macromolecules."Entry" = %s''', [entry])
-    print(len(db_cur.fetchall()))
+    db_cur.execute('''SELECT "ID" FROM macromolecules."Entry" where entry= %s''', [entry])
+    if len(db_cur.fetchall()) > 0:
+        match_count += 1
+    else:
+        unreleased.add(entry)
+
+if match_count < len(valid_ids):
+    logging.exception('Fewer entries were in DB than were released in ETS!\nMissing IDs: %s' % unreleased)
+
