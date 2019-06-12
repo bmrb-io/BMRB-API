62c67344c4c4e2292d6feec4fd5882f3e14216ce
Jon Wedell <jonwedell@gmail.com>
2019-06-12 09:12:54 +0200

    Parameterize the time domain data loader.

--- server/wsgi/utils/generate_timedomain_table.py
+++ server/wsgi/utils/generate_timedomain_table.py
@@ -3,16 +3,27 @@
 """Creates the time domain links table."""
 
 import os
+import optparse
 
 import psycopg2
 from psycopg2.extras import execute_values
 
-user = 'bmrb'
-host = 'manta'
-database = 'bmrbeverything'
-port = 5432
+opt = optparse.OptionParser(usage="usage: %prog", version="1.0",
+                            description="Reload the time domain data table.")
+opt.add_option("--host", action="store", dest="host",
+               default="manta", help="Hostname of the machine with the DB.")
+opt.add_option("--port", action="store", dest="port",
+               default="5432", help="Port of the machine with the DB.")
+opt.add_option("--database", action="store", dest="db",
+               default="bmrbeverything", help="The name of the database that the data resides in.")
+opt.add_option("--user", action="store", dest="user",
+               default="bmrb", help="The user to use to connect to the database.")
+opt.add_option("--timedomain-folder", action="store", dest="td_folder",
+               default='/website/ftp/pub/bmrb/timedomain/',
+               help="The location of the time domain data.")
+# Parse the command line input
+(options, cmd_input) = opt.parse_args()
 
-timedomain_path = '/website/ftp/pub/bmrb/timedomain/'
 
 def get_dir_size(start_path='.'):
     total_size = 0
@@ -21,6 +32,7 @@ def get_dir_size(start_path='.'):
             fp = os.path.join(dir_path, f)
             total_size += os.path.getsize(fp)
     return total_size
+
 
 def get_data_sets(path):
     sets = 0
@@ -35,7 +47,9 @@ def get_data_sets(path):
             return child_sets
     return sets
 
-conn = psycopg2.connect(user=user, host=host, database=database, port=port)
+
+conn = psycopg2.connect(user=options.user, host=options.host, database=options.db,
+                        port=options.port)
 cur = conn.cursor()
 
 cur.execute('''
@@ -44,11 +58,13 @@ bmrbid text PRIMARY KEY,
 size numeric,
 sets numeric);''')
 
+
 def td_data_getter():
-    td_dir = timedomain_path
+    td_dir = options.td_folder
     for x in os.listdir(td_dir):
         entry_id = int("".join([_ for _ in x if _.isdigit()]))
         yield (entry_id, get_dir_size(os.path.join(td_dir, x)), get_data_sets(os.path.join(td_dir, x)))
+
 
 execute_values(cur, '''INSERT INTO web.timedomain_data_tmp(bmrbid, size, sets) VALUES %s;''', td_data_getter())
 
