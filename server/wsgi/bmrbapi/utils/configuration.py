14020a683209788deeb3bb89f8353c6767c0aaa1
Jon Wedell <wedell@bmrb.wisc.edu>
2019-12-03 14:28:49 -0600

    Update configuration loader.

--- server/wsgi/bmrbapi/utils/configuration.py
+++ server/wsgi/bmrbapi/utils/configuration.py
@@ -1,10 +1,7 @@
-# Load the configuration file
 import os
 
 import simplejson as json
 
 _querymod_dir = os.path.dirname(os.path.realpath(__file__))
-_config_loc = os.path.join(_querymod_dir, "..", "..", "..", "..", "api_config.json")
-if not os.path.isfile(_config_loc):
-    _config_loc = os.path.join(_querymod_dir, "..", "configuration.json")
+_config_loc = os.path.join(_querymod_dir, "..", "configuration.json")
 configuration = json.loads(open(_config_loc, "r").read())
