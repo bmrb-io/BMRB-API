7b1df6b7c28c2cebf3f914366cb4ae629e118472
Jon Wedell <jonwedell@gmail.com>
2019-12-12 09:33:26 -0600

    Add __all__ definition to molprobity models.

--- server/wsgi/bmrbapi/schemas/molprobity.py
+++ server/wsgi/bmrbapi/schemas/molprobity.py
@@ -2,6 +2,8 @@ from marshmallow import fields
 
 from bmrbapi.schemas.default import JSONResponseSchema
 
+__all__ = ['MolprobityResidue']
+
 
 class MolprobityResidue(JSONResponseSchema):
     """ A MolProbity residue specific search"""
