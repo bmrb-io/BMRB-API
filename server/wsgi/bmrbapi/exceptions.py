62b13ba770970c1b764dad31692fb736cd786b06
Jon Wedell <wedell@bmrb.wisc.edu>
2020-01-03 13:19:54 -0600

    Add more annotations.

--- server/wsgi/bmrbapi/exceptions.py
+++ server/wsgi/bmrbapi/exceptions.py
@@ -1,24 +1,23 @@
-from flask import jsonify
+from flask import jsonify, Response
 
 
 class APIException(Exception):
 
-    def __init__(self, message, status_code=None, payload=None):
+    def __init__(self, message: str, status_code: int = None, payload: dict = None):
         Exception.__init__(self)
         self.message = message
         if status_code is not None:
             self.status_code = status_code
         self.payload = payload
 
-    def to_dict(self):
+    def to_dict(self) -> dict:
         """ Converts the payload to a dictionary."""
         rv = dict(self.payload or ())
         rv['error'] = self.message
         return rv
 
-    def to_response(self):
-        """ Returns a Flask Response object that can be
-        returned in a view. """
+    def to_response(self) -> Response:
+        """ Returns a Flask Response object that can be returned in a view. """
 
         response = jsonify(self.to_dict())
         response.status_code = self.status_code
