5187228cdce3bbc7d1a95782eb6827deeb9b1b73
Jon Wedell <wedell@bmrb.wisc.edu>
2020-01-06 14:16:36 -0600

    Make the subsequent pip upgrade commands quiet.

--- server/wsgi/setup_virtualenv.sh
+++ server/wsgi/setup_virtualenv.sh
@@ -8,5 +8,5 @@ if [[ ! -d "${SCRIPT_DIR}/virtual_env" ]]; then
   pip3 install -r ${SCRIPT_DIR}/requirements.txt
 else
   source "${SCRIPT_DIR}"/virtual_env/bin/activate
-  pip3 install -r ${SCRIPT_DIR}/requirements.txt
+  pip3 install -r ${SCRIPT_DIR}/requirements.txt --quiet
 fi
