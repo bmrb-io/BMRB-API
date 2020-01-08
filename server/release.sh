62b59230186f64ecac4dada611cc6f882250a9a1
Jon Wedell <jonwedell@gmail.com>
2020-01-08 10:34:25 -0600

    Bind the postgres socket on the production docker run arguments as well.

--- server/release.sh
+++ server/release.sh
@@ -19,8 +19,8 @@ sudo docker tag webapi pike.bmrb.wisc.edu:5000/webapi
 sudo docker push pike.bmrb.wisc.edu:5000/webapi
 
 if [[ $1 == "production" ]]; then
-    echo "sudo docker pull pike.bmrb.wisc.edu:5000/webapi; sudo docker stop webapi; sudo docker rm webapi; sudo docker run -d --name webapi -p 9002:9000 -p 9003:9001 --restart=always -v /websites/webapi/logs:/opt/wsgi/logs -v /websites/webapi/configuration.json:/opt/wsgi/bmrbapi/configuration.json pike.bmrb.wisc.edu:5000/webapi" | ssh web@herring
-    echo "sudo docker pull pike.bmrb.wisc.edu:5000/webapi; sudo docker stop webapi; sudo docker rm webapi; sudo docker run -d --name webapi -p 9002:9000 -p 9003:9001 --restart=always -v /websites/webapi/logs:/opt/wsgi/logs -v /websites/webapi/configuration.json:/opt/wsgi/bmrbapi/configuration.json pike.bmrb.wisc.edu:5000/webapi" | ssh web@blenny
+    echo "sudo docker pull pike.bmrb.wisc.edu:5000/webapi; sudo docker stop webapi; sudo docker rm webapi; sudo docker run -d --name webapi -p 9002:9000 -p 9003:9001 --restart=always -v /tmp/.s.PGSQL.5432:/tmp/.s.PGSQL.5432 -v /websites/webapi/logs:/opt/wsgi/logs -v /websites/webapi/configuration.json:/opt/wsgi/bmrbapi/configuration.json pike.bmrb.wisc.edu:5000/webapi" | ssh web@herring
+    echo "sudo docker pull pike.bmrb.wisc.edu:5000/webapi; sudo docker stop webapi; sudo docker rm webapi; sudo docker run -d --name webapi -p 9002:9000 -p 9003:9001 --restart=always -v /tmp/.s.PGSQL.5432:/tmp/.s.PGSQL.5432 -v /websites/webapi/logs:/opt/wsgi/logs -v /websites/webapi/configuration.json:/opt/wsgi/bmrbapi/configuration.json pike.bmrb.wisc.edu:5000/webapi" | ssh web@blenny
 else
     echo "sudo docker pull pike.bmrb.wisc.edu:5000/webapi; sudo docker stop webapi-devel; sudo docker rm webapi-devel; sudo docker run -d --name webapi-devel -p 9004:9000 -p 9005:9001 --restart=always -v /tmp/.s.PGSQL.5432:/tmp/.s.PGSQL.5432 -v /websites/webapi/logs:/opt/wsgi/logs -v /websites/webapi/configuration_debug.json:/opt/wsgi/bmrbapi/configuration.json pike.bmrb.wisc.edu:5000/webapi" | ssh web@blenny
 fi
