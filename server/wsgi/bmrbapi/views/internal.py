75ac169eed795130367df2896985503a656bb2da
Jon Wedell <wedell@bmrb.wisc.edu>
2020-01-17 13:54:56 -0600

    No longer need uniprot reload endpoint.

--- server/wsgi/bmrbapi/views/internal.py
+++ server/wsgi/bmrbapi/views/internal.py
@@ -1,7 +1,4 @@
-from flask import Blueprint, jsonify, redirect, url_for
-
-from bmrbapi.reloaders.uniprot import uniprot
-from bmrbapi.utils.decorators import require_local
+from flask import Blueprint, redirect, url_for
 
 internal_endpoints = Blueprint('internal', __name__)
 
@@ -12,11 +9,3 @@ def favicon_internal():
 
     return redirect(url_for('static', filename='favicon.ico'))
 
-
-@internal_endpoints.route('/refresh/uniprot')
-@require_local
-def refresh_uniprot_internal():
-    """ Refresh the UniProt links. """
-
-    uniprot()
-    return jsonify(True)
