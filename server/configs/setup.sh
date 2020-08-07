6d1e556cab956c4b76b6e7801b020ad3f4d0c244
jon@jonwedell.com <wedell@bmrb.wisc.edu>
2020-08-07 15:20:30 -0500

    Update setup script with docker launch command

--- server/configs/setup.sh
+++ server/configs/setup.sh
@@ -4,10 +4,7 @@
 yum install redis python-pip python-virtualenv mod_evasive apache-gzip postgresql10-contrib
 
 # Configure redis installation
-systemctl enable redis
-systemctl start redis
-systemctl enable redis-sentinel
-systemctl start redis-sentinel
+docker run --name redis -p 6379:6379 -v /var/lib/redis:/data --restart=always -d redis
 
 # Build FASTA
 cd ../wsgi/submodules/fasta36/src
