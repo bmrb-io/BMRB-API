65234a5792d52c50c5f1e3998fca9997454e2ecd
jon@jonwedell.com <wedell@bmrb.wisc.edu>
2020-09-16 10:31:30 -0500

    Remove the code to recreate the pdb_link table, since it should be handled elsewhere in the DB reload process.

--- server/wsgi/bmrbapi/views/metadata.py
+++ server/wsgi/bmrbapi/views/metadata.py
@@ -32,8 +32,16 @@ def get_release_statistics() -> Response:
         # Do the queries
         total_released_in_year = get_query_results(cur, released_in_year)
         original_released_in_year = get_query_results(cur, original_release_in_year)
-        structure_released_in_year = get_query_results(cur, structure_in_year)
-        nonstructure_released_in_year = get_query_results(cur, nonstructure_in_year)
+
+        structure_total_released_in_year = get_query_results(cur, structure_total_in_year)
+        structure_adit_nmr_released_in_year = get_query_results(cur, structure_aditnmr_in_year)
+        structure_onedep_released_in_year = get_query_results(cur, structure_onedep_in_year)
+        structure_bmrbdep_released_in_year = get_query_results(cur, structure_bmrbdep_in_year)
+
+        nonstructure_total_released_in_year = get_query_results(cur, nonstructure_total_in_year)
+        nonstructure_adit_nmr_released_in_year = get_query_results(cur, nonstructure_aditnmr_in_year)
+        nonstructure_onedep_released_in_year = get_query_results(cur, nonstructure_onedep_in_year)
+        nonstructure_bmrbdep_released_in_year = get_query_results(cur, nonstructure_bmrbdep_in_year)
 
         def lookup_year(res, search_year: int):
             """ Returns the number of entries for a year from a list. """
@@ -43,24 +51,37 @@ def get_release_statistics() -> Response:
             return 0
 
         # Use this do to the calculations
-        release_by_year = {'total_released_by_year': 0,
-                           'original_released_by_year': 0,
-                           'structure_released_by_year': 0,
-                           'nonstructure_released_by_year': 0}
+        total_released_by_year = 0
+        original_released_by_year = 0
+        structure_released_by_year = 0
+        nonstructure_released_by_year = 0
+
         for year in years_to_show:
-            release_by_year['total_released_by_year'] += lookup_year(total_released_in_year, year)
-            release_by_year['original_released_by_year'] += lookup_year(original_released_in_year, year)
-            release_by_year['structure_released_by_year'] += lookup_year(structure_released_in_year, year)
-            release_by_year['nonstructure_released_by_year'] += lookup_year(nonstructure_released_in_year, year)
+            total_released_by_year += lookup_year(total_released_in_year, year)
+            original_released_by_year += lookup_year(original_released_in_year, year)
+
+            structure_released_by_year += lookup_year(structure_total_released_in_year, year)
+            nonstructure_released_by_year += lookup_year(nonstructure_total_released_in_year, year)
+
             results[year] = {
-                'total_released_by_year': release_by_year['total_released_by_year'],
-                'original_released_by_year': release_by_year['original_released_by_year'],
-                'structure_released_by_year': release_by_year['structure_released_by_year'],
-                'nonstructure_released_by_year': release_by_year['nonstructure_released_by_year'],
+                'total_released_by_year': total_released_by_year,
+                'original_released_by_year': original_released_by_year,
+                'structure_released_by_year': structure_released_by_year,
+                'nonstructure_released_by_year': nonstructure_released_by_year,
                 'released_in_year': lookup_year(total_released_in_year, year),
                 'original_release_in_year': lookup_year(original_released_in_year, year),
-                'structure_release_in_year': lookup_year(structure_released_in_year, year),
-                'nonstructure_release_in_year': lookup_year(nonstructure_released_in_year, year)
+                'structure_release_in_year': {
+                    'total': lookup_year(structure_total_released_in_year, year),
+                    'adit-nmr': lookup_year(structure_adit_nmr_released_in_year, year),
+                    'onedep': lookup_year(structure_onedep_released_in_year, year),
+                    'bmrbdep': lookup_year(structure_bmrbdep_released_in_year, year)
+                },
+                'nonstructure_release_in_year': {
+                    'total': lookup_year(nonstructure_total_released_in_year, year),
+                    'adit-nmr': lookup_year(nonstructure_adit_nmr_released_in_year, year),
+                    'onedep': lookup_year(nonstructure_onedep_released_in_year, year),
+                    'bmrbdep': lookup_year(nonstructure_bmrbdep_released_in_year, year)
+                },
             }
 
     return jsonify({'release_information': results})
--- server/wsgi/bmrbapi/views/sql/metadata.py
+++ server/wsgi/bmrbapi/views/sql/metadata.py
@@ -12,19 +12,90 @@ WHERE newstatus LIKE 'rel'
 GROUP BY date_part('year', logdate)
 ORDER BY date_part('year', logdate);"""
 
-structure_in_year: str = """
+structure_total_in_year: str = """
 SELECT date_part('year', release_date), count(*)
 FROM entrylog
 WHERE status LIKE 'rel%%'
   AND pdb_code IS NOT NULL
   AND pdb_code NOT IN ('?', '.', '')
 GROUP BY date_part('year', release_date)
-ORDER BY date_part('year', release_date);"""
+ORDER BY date_part('year', release_date);
+"""
 
-nonstructure_in_year: str = """
+structure_aditnmr_in_year: str = """
+SELECT date_part('year', release_date), count(*)
+FROM entrylog
+WHERE status LIKE 'rel%%'
+  AND LENGTH(nmr_dep_code) = 7
+  AND pdb_code IS NOT NULL
+  AND pdb_code NOT IN ('?', '.', '')
+GROUP BY date_part('year', release_date)
+ORDER BY date_part('year', release_date);
+"""
+
+structure_onedep_in_year: str = """
+SELECT date_part('year', release_date), count(*)
+FROM entrylog
+WHERE status LIKE 'rel%%'
+  AND LENGTH(nmr_dep_code) = 12
+  AND pdb_code IS NOT NULL
+  AND pdb_code NOT IN ('?', '.', '')
+GROUP BY date_part('year', release_date)
+ORDER BY date_part('year', release_date);
+"""
+
+structure_bmrbdep_in_year: str = """
+SELECT date_part('year', release_date), count(*)
+FROM entrylog
+WHERE status LIKE 'rel%%'
+  AND nmr_dep_code = restart_id
+  AND pdb_code IS NOT NULL
+  AND pdb_code NOT IN ('?', '.', '')
+GROUP BY date_part('year', release_date)
+ORDER BY date_part('year', release_date);
+"""
+
+
+
+
+
+
+
+nonstructure_total_in_year: str = """
 SELECT date_part('year', release_date), count(*)
 FROM entrylog
 WHERE status LIKE 'rel%%'
   AND (pdb_code IS NULL OR pdb_code IN ('?', '.', ''))
 GROUP BY date_part('year', release_date)
-ORDER BY date_part('year', release_date);"""
+ORDER BY date_part('year', release_date);
+"""
+
+nonstructure_aditnmr_in_year: str = """
+SELECT date_part('year', release_date), count(*)
+FROM entrylog
+WHERE status LIKE 'rel%%'
+  AND LENGTH(nmr_dep_code) = 7
+  AND (pdb_code IS NULL OR pdb_code IN ('?', '.', ''))
+GROUP BY date_part('year', release_date)
+ORDER BY date_part('year', release_date);
+"""
+
+nonstructure_onedep_in_year: str = """
+SELECT date_part('year', release_date), count(*)
+FROM entrylog
+WHERE status LIKE 'rel%%'
+  AND LENGTH(nmr_dep_code) = 12
+  AND (pdb_code IS NULL OR pdb_code IN ('?', '.', ''))
+GROUP BY date_part('year', release_date)
+ORDER BY date_part('year', release_date);
+"""
+
+nonstructure_bmrbdep_in_year: str = """
+SELECT date_part('year', release_date), count(*)
+FROM entrylog
+WHERE status LIKE 'rel%%'
+  AND nmr_dep_code = restart_id
+  AND (pdb_code IS NULL OR pdb_code IN ('?', '.', ''))
+GROUP BY date_part('year', release_date)
+ORDER BY date_part('year', release_date);
+"""
