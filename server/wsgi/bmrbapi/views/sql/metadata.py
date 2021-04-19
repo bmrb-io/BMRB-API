701e7cf19d72c9732ef8bae0a8c374d09780032a
jon@jonwedell.com <wedell@bmrb.wisc.edu>
2021-04-19 15:25:42 -0500

    Update the release queries to be more precise by using the logtable rather than the entrylog.

--- server/wsgi/bmrbapi/views/sql/metadata.py
+++ server/wsgi/bmrbapi/views/sql/metadata.py
@@ -1,23 +1,39 @@
 released_in_year: str = """
+SELECT lt.year, count(*)
+FROM entrylog AS el
+         LEFT JOIN
+     (SELECT depnum, date_part('year', logdate) AS year
+      FROM logtable
+      WHERE newstatus = 'rel'
+      GROUP BY depnum, year) AS lt ON el.depnum = lt.depnum
+WHERE lt.depnum IS NOT NULL
+GROUP BY lt.year
+ORDER BY lt.year;"""
+
+original_release_in_year: str = """
 SELECT date_part('year', release_date), count(*)
-FROM entrylog
-WHERE status LIKE 'rel%%'
+FROM entrylog AS el
+         LEFT JOIN
+     (SELECT depnum, min(logdate) AS logdate
+      FROM logtable
+      WHERE newstatus = 'rel'
+      GROUP BY depnum) AS lt ON el.depnum = lt.depnum
+WHERE el.status LIKE 'rel%'
 GROUP BY date_part('year', release_date)
 ORDER BY date_part('year', release_date);"""
 
-original_release_in_year: str = """
-SELECT date_part('year', logdate), count(*)
-FROM logtable
-WHERE newstatus LIKE 'rel'
-GROUP BY date_part('year', logdate)
-ORDER BY date_part('year', logdate);"""
-
 structure_total_in_year: str = """
 SELECT date_part('year', release_date), count(*)
-FROM entrylog
-WHERE status LIKE 'rel%%'
-  AND pdb_code IS NOT NULL
-  AND pdb_code NOT IN ('?', '.', '')
+FROM entrylog AS el
+         LEFT JOIN
+     (SELECT depnum, min(logdate) AS logdate
+      FROM logtable
+      WHERE newstatus = 'rel'
+      GROUP BY depnum) AS lt ON el.depnum = lt.depnum,
+     unnest(string_to_array(el.pdb_code, ' ')) s(pdb_code)
+WHERE el.pdb_code IS NOT NULL
+  AND el.pdb_code NOT IN ('?', '.', '')
+  AND s.pdb_code !~ '^[0-9]+$'
 GROUP BY date_part('year', release_date)
 ORDER BY date_part('year', release_date);
 """
