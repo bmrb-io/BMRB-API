7d5241a8c2a51cfba2f640b1adb245974fdc7eb2
Jon Wedell <wedell@bmrb.wisc.edu>
2022-11-16 16:10:13 -0600

    Time to call it a day.

--- server/wsgi/bmrbapi/reloaders/__main__.py
+++ server/wsgi/bmrbapi/reloaders/__main__.py
@@ -192,7 +192,7 @@ if options.metabolomics:
     logger.info('Calculating metabolomics entries to process...')
     with PostgresConnection() as cur:
         cur.execute('SELECT DISTINCT "ID" FROM metabolomics."Entry" ORDER BY "ID"')
-        entries = sorted([x['Entry_ID'] for x in cur.fetchall()])
+        entries = sorted([x['ID'] for x in cur.fetchall()])
 
     if len(entries) < 1000:
         raise ValueError("Refusing to continue, the DB appears corrupted.")
