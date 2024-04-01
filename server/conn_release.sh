1d384ff939bb2394ff87554c688b8c9706bd9778
Jon Wedell <wedell@bmrb.wisc.edu>
2024-04-01 15:45:32 -0500

    Make the timedomain deposition directory available.

--- server/conn_release.sh
+++ server/conn_release.sh
@@ -13,9 +13,10 @@ fi
 echo "Deploying docker instance..."
 docker stop webapi
 docker rm webapi
-docker run -d --name webapi -p 9005:9001 -p9006:9000 --restart=always --link redis:redis \
+docker run -d --name webapi -p 9005:9001 -p9006:9000 --restart=always --link redis:redis --user 17473:10144 \
  --sysctl net.core.somaxconn=1024 \
  -v /tmp/logs:/opt/wsgi/logs \
  -v /tmp/.s.PGSQL.5432:/tmp/.s.PGSQL.5432 \
  -v /projects/BMRB/public/api/configuration.json:/opt/wsgi/bmrbapi/configuration.json \
+ -v /projects/BMRB/depositions/timedomain_upload:/projects/BMRB/depositions/timedomain_upload \
  webapi
