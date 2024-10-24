267db9ff3270506d343a4b1e7c3b30b68557ef9a
Jon Wedell <wedell@bmrb.wisc.edu>
2024-10-24 14:56:41 -0500

    Update packages (especially lxml which won't install at the version it was on python3.12) and switch to using the standard venv naming convention.

--- server/wsgi/setup_virtualenv.sh
+++ server/wsgi/setup_virtualenv.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-if [[ ! -d "${SCRIPT_DIR}/virtual_env" ]]; then
-  python3 -m venv ${SCRIPT_DIR}/virtual_env
-  source "${SCRIPT_DIR}"/virtual_env/bin/activate
+if [[ ! -d "${SCRIPT_DIR}/venv" ]]; then
+  python3 -m venv ${SCRIPT_DIR}/venv
+  source "${SCRIPT_DIR}"/venv/bin/activate
   pip3 install --upgrade pip
   pip3 install -r ${SCRIPT_DIR}/requirements.txt
 else
-  source "${SCRIPT_DIR}"/virtual_env/bin/activate
+  source "${SCRIPT_DIR}"/venv/bin/activate
   pip3 install --upgrade pip --quiet
   pip3 install -r ${SCRIPT_DIR}/requirements.txt --quiet
 fi
